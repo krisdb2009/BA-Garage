c61c6e55929f77d0ce25d9f9f8fa5dc49ac00066
Dylan Bickerstaff <dylan@belowaverage.org>
2022-05-30 03:13:34 -0400

    Add project files.

---
+++ BA-Garage/Classes/GarageDriver.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Sockets;
+using System.Net;
+
+namespace BA_Garage
+{
+    public static class GarageDriver
+    {
+        public static UdpClient UdpClient = new UdpClient();
+        public static void Trigger(byte RelayIndex)
+        {
+            IPEndPoint ipe = new IPEndPoint(IPAddress.Parse(Properties.Settings.Default.RelayIP), 60000);
+            byte onWithIndex = (byte)((RelayIndex << 1) | 0b00000001);
+            UdpClient.Send(new byte[] { 0xff, 0xaa, 0x0, 0x3, 0x0, 0x0, onWithIndex }, 7, ipe);
+        }
+    }
+}
